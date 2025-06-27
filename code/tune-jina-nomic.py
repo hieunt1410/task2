3f182ec5a52d2a47f78e6b5ba7fc5a312ef2e6f6
hieunt1410 <hieungo1410@gmail.com>
2025-06-27 07:17:11 +0000

    modify code

--- code/tune-jina-nomic.py
+++ code/tune-jina-nomic.py
@@ -37,6 +37,7 @@ device = torch.device('cuda', local_rank)
 BERT_LEARNING_RATE = 5e-5
 EPOCH, BATCH_SIZE = 10, 2
 EVALUATION_PER_STEP = 300
+MAX_SEQUENCE_LENGTH = 8192
 
 def set_seed(seed=777):
     random.seed(seed)
